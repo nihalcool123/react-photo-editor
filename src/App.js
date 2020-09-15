d67e1916ea56233b40b9324c1b8b9fd6cd650e74
nihalcool123 <nihalmah123@gmail.com>
2020-09-15 13:07:32 +0530

    Sidebar changes and some functionalities

--- src/App.js
+++ src/App.js
@@ -1,17 +1,114 @@
-import React from "react";
-import Slider from "./Slider"
+import React, { useState } from "react";
+import Slider from "./Slider";
 import "./style.css";
+import SidebarItem from "./SidebarItem";
+
+const DEFAULT_OPTIONS = [
+  {
+    name: "Brightness",
+    property: "brightness",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: "%"
+  },
+  {
+    name: "Contrast",
+    property: "contrast",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: "%"
+  },
+  {
+    name: "Saturation",
+    property: "saturation",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: "%"
+  },
+  {
+    name: "Grayscale",
+    property: "grayscale",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: "%"
+  },
+  {
+    name: "Brightness",
+    property: "brightness",
+    value: 100,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: "%"
+  },
+  {
+    name: "Sepia",
+    property: "sepia",
+    value: 100,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: "%"
+  },
+  {
+    name: "Hue Rotate",
+    property: "hue-rotate",
+    value: 100,
+    range: {
+      min: 0,
+      max: 360
+    },
+    unit: "deg"
+  },
+  {
+    name: "Blur",
+    property: "blur",
+    value: 100,
+    range: {
+      min: 0,
+      max: 20
+    },
+    unit: "px"
+  }
+];
 
 export default function App() {
-  return (
-    <div className="container" >
-    <div className="main-image">Image</div>
-    <div className="sidebar" >
-    Sidebar
-    </div>
+  const [options, setOptions] = useState(DEFAULT_OPTIONS);
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
 
-    <Slider />
-      
+  const selectedOption = options[selectedOptionIndex];
+
+  return (
+    <div className="container">
+      <div className="main-image" />
+      <div className="sidebar">
+        {options.map((option, index) => {
+          return (
+            <SidebarItem
+              key={index}
+              name={option.name}
+              active={index === selectedOptionIndex}
+              handleClick={() => setSelectedOptionIndex(index)}
+            />
+          );
+        })}
+      </div>
+
+      <Slider />
     </div>
   );
 }
