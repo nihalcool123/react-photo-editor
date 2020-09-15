34140110c6898101f28d93a89b233e973700a579
nihalcool123 <nihalmah123@gmail.com>
2020-09-15 10:54:10 +0530

    css grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Slider from "./Slider"
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container" >
+    <div className="main-image">Image</div>
+    <div className="sidebar" >
+    Sidebar
+    </div>
+
+    <Slider />
+      
     </div>
   );
 }
